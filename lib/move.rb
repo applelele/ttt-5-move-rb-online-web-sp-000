0091a09587d131032542854cfed3e07063c74f11
YUKIKO IKEDA <applelele@gmail.com>
2020-06-10 02:58:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,9 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  user_move = input.to_i - 1
+end
+
+def move(user_move, players_character, players_character = "X")
+end
